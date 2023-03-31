453023c94f0bc4d5e526220aefd26cab4b614749
param108 <param.ponnaiyan@gmail.com>
2023-03-31 13:04:36 +0530

    Added code to do bumping

---
+++ hero_k.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+const WALK_SPEED = 2
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_left"):
+		velocity = Vector2.LEFT*WALK_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity =  Vector2.RIGHT*WALK_SPEED
+	elif Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP*WALK_SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity = Vector2.DOWN*WALK_SPEED
+	else:
+		velocity = Vector2.ZERO
+	move_and_collide(velocity)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
