0c47110f6c720d43c71a56342dcfaa7991a596a0
paramspace <param@kawa.space>
2023-04-11 19:34:36 +0530

    dded the dialog

--- singletons/GlobalData.gd
+++ singletons/GlobalData.gd
@@ -11,6 +11,17 @@ const dialog_data = {
 		"dialog": "Sorry didn't understand the output.",
 		"need_input": true,
 		"choices": {},
+	},
+	"aria_clue":{
+		"dialog": """Hallo ! You must be ‘The Dr, Zoey ‘ . My neighbor and best friend , Saira , told me you were coming to find
+		the Treasure of the Kirghiz . Well , I am Aria ,nice to meet you . My friend Saira or ‘The Fisher’ told me that you
+		would be coming in search of the map fragment.
+
+		I can't help you much, but my parents taught me this riddle long long ago.
+		My house has 2 bedrooms , 1 guest room and 1 kitchen . in one of these rooms lies a not belonging object locate it and you will 
+		get the fragment """,
+		"need_input" : false,
+		"choices" : {}
 	}
 }
 
